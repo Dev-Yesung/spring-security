ca8cdc73ceceb00dfb2e1bb68bd3e384d720c7e0
ysng_ysng <yeasung67@gmail.com>
2023-07-31 21:04:43 +0900

    feat: 스프링 시큐리티 미션1-1 완료
    - passwordEncoder는 NoOpPasswordEncoder로 사용함 (힌트:
    DelegatingPasswordEncoder)
    - 기본 로그인 계정을 AuthenticationManagerBuilder
    클래스를 통해 추가
    
    스프링6에 맞는 스프링 시큐리티 코드로 바꿔서 구현했습니다.

--- src/main/java/com/prgrms/devcourse/configures/WebMvcConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebMvcConfigure.java
@@ -1,8 +1,10 @@
 package com.prgrms.devcourse.configures;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfigure implements WebMvcConfigurer {
 
     @Override
